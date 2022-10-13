eceed5865891109e928f9a25e58efeee4133ed4d
Developer Rakib <githubs.pro@gmail.com>
2022-10-14 02:46:02 +0600

    hello new git

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+   
+    $(".typed").typed({
+        strings: [" I am Freelancer Rakib", " I am Front-End Web developer ", " I am Graphics Designer " ,],
+        loop: true,
+        typeSpeed: 30,
+        startDelay: 1200,
+        backSpeed: 20,
+        backDelay: 500,
+        loopCount: 500,
+        showCursor: false,
+    })
+    
+});
