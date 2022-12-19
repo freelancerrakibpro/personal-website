81cc5ed858a1c6987059a9263a4c94a75e13cc5b
Developer Rakib <githubs.pro@gmail.com>
2022-12-19 20:09:41 +0600

    hello mail

--- mail.php
+++ mail.php
@@ -9,7 +9,7 @@ $to = "rakibs232309@gmail.com";
 $subject = "Mail From website codeconia";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n mobile number =" . $number . "\r\n message = " . $message;
 
-$headers = "From: rakib232309@gmail.com" . "\r\n" .
+$headers = "From: noreply@codeconia.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
